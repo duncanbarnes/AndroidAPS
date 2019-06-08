219417c76f52e3cffe273b264259019bb7a55179
Duncan Barnes <github@duncanbarnes.net>
2019-06-08 21:10:20 +0100

    Update Constants.java
    Updated accuracy of MMOLL_TO_MGDL conversion to match xDrip constant, so that there isn't an inconsistency between the two (albeit only ever 0.1 difference).

--- app/src/main/java/info/nightscout/androidaps/Constants.java
+++ app/src/main/java/info/nightscout/androidaps/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String MGDL = "mg/dl"; // This is Nightscout's representation
     public static final String MMOL = "mmol";
 
-    public static final double MMOLL_TO_MGDL = 18; // 18.0182;
+    public static final double MMOLL_TO_MGDL = 18.0182;
     public static final double MGDL_TO_MMOLL = 1 / MMOLL_TO_MGDL;
 
     public static final double defaultDIA = 5d;
